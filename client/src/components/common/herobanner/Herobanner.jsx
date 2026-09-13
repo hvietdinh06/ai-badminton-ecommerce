6eee8f2c00af7c813a6ae51067f17dc009cd15f1
Hoang Viet Dinh <hoangvietdinh0506@gmail.com>
2026-09-14 00:56:38 +0700

    feat: integrate Swiper into HeroBanner component

--- client/src/components/common/herobanner/Herobanner.jsx
+++ client/src/components/common/herobanner/Herobanner.jsx
@@ -1,48 +1,36 @@
-import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
 import "./herobanner.css";
 
 import banner1 from "../../../assets/images/banner1.png";
 import banner2 from "../../../assets/images/banner2.png";
 import banner3 from "../../../assets/images/banner3.png";
 
-const slides = [banner1, banner2, banner3];
-
 function HeroBanner() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) =>
-        prev === slides.length - 1 ? 0 : prev + 1
-      );
-    }, 4000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <section className="hero-banner">
-      <div className="hero-slider">
-        <img
-          src={slides[currentSlide]}
-          alt="Badminton promotion"
-          className="hero-image"
-        />
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        loop={true}
+        className="hero-swiper"
+      >
+        <SwiperSlide>
+          <img src={banner1} alt="Banner 1" className="hero-image" />
+        </SwiperSlide>
 
-        <div className="hero-dots">
-          {slides.map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              className={`hero-dot ${
-                currentSlide === index ? "active" : ""
-              }`}
-              onClick={() => setCurrentSlide(index)}
-              aria-label={`Slide ${index + 1}`}
-            />
-          ))}
-        </div>
-      </div>
+        <SwiperSlide>
+          <img src={banner2} alt="Banner 2" className="hero-image" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src={banner3} alt="Banner 3" className="hero-image" />
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
